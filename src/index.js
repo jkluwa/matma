77fe79fab42caf094521ee4bf65ff9ec0d107a53
jkluwa <jkluwa4@gmail.com>
2021-12-02 22:18:31 +0100

    fetch fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const PORT = process.env.PORT || 5000
+
 ReactDOM.render(
   <React.StrictMode>
     <style>{`
