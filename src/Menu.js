d825bc31e7e93f57af8743936b8ec7229932a918
jkluwa <jkluwa4@gmail.com>
2021-12-04 21:03:32 +0100

    matma

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,7 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react"
 import styled from "styled-components"
+import User from 'User'
+import Admin from 'Admin'
 
 
 
@@ -11,64 +13,25 @@ const Form = styled.form`
   padding: 20px 50px;
   text-align: center;
 `
-const Button = styled.button`
-  background-color: #bcbbed;
-  border: 3px #9a99cb solid;
-  color: white;
-  border-radius: 20px;
-  padding: 5px 10px;
-`
+
 
 
 const Menu = () => {
-    const [isGameAvaible, setIsGameAvaible] = useState(true);
-  const nameRef = useRef("");
-  const passwordRef = useRef("");
-  let form = "";
+  const [isUser, setIsUser] = useState(true)
+  const [isGameAvaible, setIsGameAvaible] = useState(true)
 
-  useEffect(() => {
-  }, [])
-
-
-  const startHandler = (event) => {
-    event.preventDefault()
-    fetch('https://matma-backend.herokuapp.com/users/create/', {
-      method: 'post',
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "name": nameRef.current.value })}).then((res) => {
-        console.log(res);
-      })
+  const changeUser = () => {
+    setIsUser((user) => {
+      return !user
+    }
+    )
   }
-
-  const startAdminHandler = (event) => {
-    event.preventDefault()
-  }
-
   
-  const adminHandler = (event) => {
-    event.preventDefault()
-    form = <Form>
-    <h1>Wpisz hasło</h1>
-    <input type="password" ref={passwordRef}/>
-    <Button onClick={startAdminHandler}>Start</Button>
-    <Button onClick={() => {setIsGameAvaible(true)}}>Jestem użytkownikiem</Button>
-  </Form>
-  }
-  if(isGameAvaible) {
-    form = <Form>
-    <h1>Wpisz swoją nazwę</h1>
-    <input ref={nameRef}/>
-    <Button onClick={startHandler}>Start</Button>
-    <Button onClick={adminHandler}>Jestem adminem</Button>
-  </Form>
-  } else {
-    form =  <Form>
-              <h1>Gra nie jest dostępna</h1>
-            </Form>
-  }
-
   
-    return <div>{form}</div>
+    return <Form>
+      {isUser && <User props={changeUser}/>}
+      {!isUser && <Admin props={changeUser} />}
+    </Form>
 }
 
 export default Menu;
