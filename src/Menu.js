5bb6dc5f4750ff286ea79512b3303544dd857ad0
jkluwa <jkluwa4@gmail.com>
2021-12-04 19:54:48 +0100

    matma

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components"
 
 
@@ -23,20 +23,40 @@ const Button = styled.button`
 const Menu = () => {
     const [isGameAvaible, setIsGameAvaible] = useState(true);
   const nameRef = useRef("");
+  const passwordRef = useRef("");
   let form = "";
-  const submitHandler = (event) => {
-    event.preventDefault();
-    console.log(nameRef.current.value);
+
+  useEffect(() => {
+  }, [])
+
+
+  const startHandler = (event) => {
+    event.preventDefault()
     fetch('https://matma-backend.herokuapp.com/users/create/', {
       method: 'post',
       headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
       body: JSON.stringify({ "name": nameRef.current.value })})
   }
+
+  const startAdminHandler = (event) => {
+    event.preventDefault()
+  }
+
+  const adminHandler = () => {
+    form = <Form>
+    <h1>Wpisz hasło</h1>
+    <input type="password" ref={passwordRef}/>
+    <Button onClick={startAdminHandler}>Start</Button>
+    <Button onClick={() => {setIsGameAvaible(true)}}>Jestem użytkownikiem</Button>
+  </Form>
+  }
+
   if(isGameAvaible) {
     form = <Form>
     <h1>Wpisz swoją nazwę</h1>
     <input ref={nameRef}/>
-    <Button onClick={submitHandler}>Start</Button>
+    <Button onClick={startHandler}>Start</Button>
+    <Button onClick={adminHandler}>Jestem adminem</Button>
   </Form>
   } else {
     form =  <Form>
