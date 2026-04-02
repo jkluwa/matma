74e8cb28713d534a4d00977f3bbca9085b1d2af1
jkluwa <jkluwa4@gmail.com>
2026-04-02 21:28:59 +0200

    chore: containarize fronted

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20-slim
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+ENV NODE_OPTIONS=--openssl-legacy-provider
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- src/AdminStart.js
+++ src/AdminStart.js
@@ -14,7 +14,6 @@ const AdminStart = (props) => {
 
     return (
         <div>
-            Dominik
             <svg viewBox="-2 -2 100 100"  width="200px">
                 <path d={`M 0 0 L 50,0 Q 60 0 60 10 L 60,${height} Q 60,${height+10} 70,${height+10}`} strokeWidth="2px" stroke="black" fill='none' />
                 <path d={`M 0 ${height*2+20} L 50,${height*2+20} Q 60 ${height*2+20} 60 ${height*2+10} L 60,${height*2+10} Q 60,${height*2} 70,${height*2}`} strokeWidth="2px" stroke="black" fill='none' />
